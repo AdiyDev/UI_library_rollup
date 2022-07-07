49d8646499db677e7e10aaa9f79ca2bf487392b3
Arkadiy B <adiy.work.mail@ya.ru>
2022-07-08 01:11:57 +0300

    feat: up new version

--- dist/esm/index.js
+++ dist/esm/index.js
@@ -2800,6 +2800,36 @@ function requireReact_development () {
 
 var React = /*@__PURE__*/getDefaultExportFromCjs(react.exports);
 
+function styleInject(css, ref) {
+  if ( ref === void 0 ) ref = {};
+  var insertAt = ref.insertAt;
+
+  if (!css || typeof document === 'undefined') { return; }
+
+  var head = document.head || document.getElementsByTagName('head')[0];
+  var style = document.createElement('style');
+  style.type = 'text/css';
+
+  if (insertAt === 'top') {
+    if (head.firstChild) {
+      head.insertBefore(style, head.firstChild);
+    } else {
+      head.appendChild(style);
+    }
+  } else {
+    head.appendChild(style);
+  }
+
+  if (style.styleSheet) {
+    style.styleSheet.cssText = css;
+  } else {
+    style.appendChild(document.createTextNode(css));
+  }
+}
+
+var css_248z = "button {\r\n  font-size: 60px;\r\n}\r\n";
+styleInject(css_248z);
+
 const Button = (props) => {
     return React.createElement("button", null, props.label);
 };
